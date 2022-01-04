9cd7fcba73794e2d264490acea678e1a0eaf698c
Dmitry Lakhov <73223982+bobrokerson@users.noreply.github.com>
2022-01-04 17:25:34 +0100

    Update assignment

--- assignment2.py
+++ assignment2.py
@@ -1,3 +1,3 @@
 # python
-name = raw_input("Enter your name, please")
+name = raw_input("Enter your name, pls")
 print ("Hey" + ' ' + name) # 'space' to add space between words, and () to make python 3 print statement
